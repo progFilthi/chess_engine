ce4c4529840049f8f88ec76379afd44414da0d71
Programmer Filthi <gamerfilthi@gmail.com>
2026-06-30 23:00:26 +0200

    Chess pieces placements on the board

--- board/board_test.go
+++ board/board_test.go
@@ -14,3 +14,18 @@ func TestBoard(t *testing.T) {
 	}
 
 }
+
+func TestStartingPosition(t *testing.T) {
+	b := NewBoard()
+
+	SetStartingPosition(&b)
+
+	if b.Pieces[6].PieceType != Knight {
+		t.Errorf("Piece on index 6 should be a Knight. (got %v)", b.Pieces[6].PieceType)
+	}
+
+	if b.Pieces[6].Color != Black {
+		t.Errorf("Piece on index 6 should be a Black Knight. (got %v)", b.Pieces[6].Color)
+	}
+
+}
